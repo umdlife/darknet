12449ffcf3dcd86c5679968b33497c25a2a09ab0
Victor Massagué Respall <victor@unmanned.life>
2022-08-17 10:48:31 +0200

    Silence printfs of classes when detection

--- src/image.c
+++ src/image.c
@@ -337,31 +337,31 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
     // text output
     qsort(selected_detections, selected_detections_num, sizeof(*selected_detections), compare_by_lefts);
     int i;
-    for (i = 0; i < selected_detections_num; ++i) {
-        const int best_class = selected_detections[i].best_class;
-        printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
-        if (ext_output)
-            printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
-                round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
-                round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2)*im.h),
-                round(selected_detections[i].det.bbox.w*im.w), round(selected_detections[i].det.bbox.h*im.h));
-        else
-            printf("\n");
-        int j;
-        for (j = 0; j < classes; ++j) {
-            if (selected_detections[i].det.prob[j] > thresh && j != best_class) {
-                printf("%s: %.0f%%", names[j], selected_detections[i].det.prob[j] * 100);
+    // for (i = 0; i < selected_detections_num; ++i) {
+    //     const int best_class = selected_detections[i].best_class;
+    //     printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
+    //     if (ext_output)
+    //         printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
+    //             round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
+    //             round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2)*im.h),
+    //             round(selected_detections[i].det.bbox.w*im.w), round(selected_detections[i].det.bbox.h*im.h));
+    //     else
+    //         printf("\n");
+    //     int j;
+    //     for (j = 0; j < classes; ++j) {
+    //         if (selected_detections[i].det.prob[j] > thresh && j != best_class) {
+    //             printf("%s: %.0f%%", names[j], selected_detections[i].det.prob[j] * 100);
 
-                if (ext_output)
-                    printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
-                        round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
-                        round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2)*im.h),
-                        round(selected_detections[i].det.bbox.w*im.w), round(selected_detections[i].det.bbox.h*im.h));
-                else
-                    printf("\n");
-            }
-        }
-    }
+    //             if (ext_output)
+    //                 printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
+    //                     round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
+    //                     round((selected_detections[i].det.bbox.y - selected_detections[i].det.bbox.h / 2)*im.h),
+    //                     round(selected_detections[i].det.bbox.w*im.w), round(selected_detections[i].det.bbox.h*im.h));
+    //             else
+    //                 printf("\n");
+    //         }
+    //     }
+    // }
 
     // image output
     qsort(selected_detections, selected_detections_num, sizeof(*selected_detections), compare_by_probs);
